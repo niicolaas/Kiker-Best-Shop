bc7a3c73c2d6ea5a8c01715b2b255283f4867409
niicolaas <contato.nicolas.dev@gmail.com>
2026-03-01 20:28:47 -0300

    :broom: cleanup: Removing commented-out code blocks and unused code

--- inertia/layouts/default.tsx
+++ inertia/layouts/default.tsx
@@ -2,61 +2,38 @@ import { Data } from '@generated/data'
 import { toast, Toaster } from 'sonner'
 import { usePage } from '@inertiajs/react'
 import { ReactElement, useEffect } from 'react'
-import { Link } from '@adonisjs/inertia/react'
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
 import { AppSidebar } from '@/components/app-sidebar'
 
 export default function Layout({ children }: { children: ReactElement<Data.SharedProps> }) {
+  const page = usePage()
+
   useEffect(() => {
     toast.dismiss()
-  }, [usePage().url])
+  }, [page.url])
 
   if (children.props.flash.error) {
     toast.error(children.props.flash.error)
+  }
+
+  const isHome = page.component === 'home'
+
+  if (isHome) {
+    return (
+      <>
+        <main className="min-h-screen bg-slate-50">
+          {children}
+        </main>
+        <Toaster position="top-center" richColors />
+      </>
+    )
   }
 
   return (
     <>
       <SidebarProvider>
         <AppSidebar />
-        <main>
-          <header>
-            <div>
-              <div>
-                <Link route="home">
-                  <svg
-                    width="120"
-                    height="24"
-                    viewBox="0 0 195 38"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M180 37.5v-30h-7.5V0H195v7.5h-7.5v30H180ZM150 15V7.5h-15V0h15v7.5h7.5V15H150Zm-15 22.5V30h-7.5V7.5h7.5V30h15v7.5h-15Zm15-7.5v-7.5h7.5V30H150ZM82.5 37.5v-30H90V0h15v7.5h7.5v30H105v-15H90v15h-7.5ZM90 15h15V7.8H90V15ZM45 37.5V0h22.5v7.5h-15V15h15v7.5h-15V30h15v7.5H45ZM0 37.5V0h22.5v7.5H30V15h-7.5v15H30v7.5h-7.5V30H15v-7.5H7.5v15H0ZM7.5 15h14.7V7.5H7.5V15Z"
-                      fill="currentColor"
-                    />
-                  </svg>
-                </Link>
-              </div>
-              {/*<div>
-            <nav>
-              {children.props.user ? (
-                <>
-                  <span>{children.props.user.initials}</span>
-                  <Form route="session.destroy">
-                    <button type="submit"> Logout </button>
-                  </Form>
-                </>
-              ) : (
-                <>
-                  <Link route="new_account.create">Registrar</Link>
-                  <Link route="session.create">Logar</Link>
-                </>
-              )}
-            </nav>
-          </div>*/}
-            </div>
-          </header>
+        <main className="min-h-screen">
           <SidebarTrigger />
           {children}
         </main>
@@ -65,3 +42,4 @@ export default function Layout({ children }: { children: ReactElement<Data.Share
     </>
   )
 }
+

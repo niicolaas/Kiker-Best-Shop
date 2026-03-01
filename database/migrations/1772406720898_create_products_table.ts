ce3d17adf84e00a5d3f4290d2a3e3fce75c03cbc
niicolaas <contato.nicolas.dev@gmail.com>
2026-03-01 20:50:35 -0300

    feat: add migration for products table

---
+++ database/migrations/1772406720898_create_products_table.ts
@@ -0,0 +1,22 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'products'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.string('name').notNullable()
+      table.decimal('price', 10, 2).notNullable()
+      table.text('description').nullable()
+      table.string('imgurl').nullable()
+
+      table.timestamp('created_at', { useTz: true }).notNullable()
+      table.timestamp('updated_at', { useTz: true }).notNullable()
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}

1c20c3dc426d5027da5ddb163b56b09e91ec7ee6
niicolaas <contato.nicolas.dev@gmail.com>
2026-03-17 21:55:10 -0300

    :truck: chore: Verify if the product exists

--- app/controllers/product/create_product_controller.ts
+++ app/controllers/product/create_product_controller.ts
@@ -1,15 +1,28 @@
 import Product from '#models/product'
 import { product_validator } from '#validators/product_validator'
 import type { HttpContext } from '@adonisjs/core/http'
+import HuggingFaceService from '#services/huggingFaceService'
 
 export default class CreateProductController {
   async create({ request, response }: HttpContext) {
+    const huggingFaceService = new HuggingFaceService()
+
     const payload = await request.validateUsing(product_validator)
 
-    const pendingCreateProduct = await Product.create(payload)
+    const productExists = await Product.findBy('name', payload.name)
 
-    if (!pendingCreateProduct) response.status(400).send('Error to create the product')
+    if (productExists)
+      return response.status(409).send({ error: 'Um produto com este nome já existe!' })
 
-    response.status(200).send('Product created with success!')
+    const context = `${payload.name}: ${payload.description}`
+    const vector = await huggingFaceService.generateEmbedding(context)
+
+    payload.embedding = JSON.stringify(vector)
+
+    const pendingProduct = await Product.create(payload)
+
+    if (!pendingProduct) response.status(400).send('Error to create the product')
+
+    response.status(201).send({ message: 'Product created with success!', product: pendingProduct })
   }
 }
